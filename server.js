773eb01e38f4dc457e0d3fc392b45d220fe89735
Danish Thomas <a.danishthomas@gmail.com>
2023-01-10 17:30:48 +0530

    working whatsapp backend with out authentication

--- server.js
+++ server.js
@@ -43,6 +43,8 @@ db.once('open',()=>{
       {
         name: messageDetails.name,
         message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        recived: messageDetails.recived
       });
     } else{
       console.log("error in pusher");
